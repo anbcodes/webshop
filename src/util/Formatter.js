c8903b81a4d55c4426b865044bb674a70dd5b468
anbcodes <anbcodes@protonmail.com>
2019-11-07 21:00:36 -0500

    Added emailing receipt

--- src/util/Formatter.js
+++ src/util/Formatter.js
@@ -2,4 +2,26 @@ export default {
   formatPrice(number) {
     return `$${parseFloat(number).toFixed(2)}`;
   },
+
+  formatStringForReceipt(str, price) {
+    const strList = str.split(' ');
+    let currentStr = '';
+    const rows = [];
+    strList.forEach((item) => {
+      if ((currentStr + item).length < 25) {
+        currentStr += item;
+      } else {
+        rows.push(currentStr);
+        currentStr = '';
+      }
+    });
+    if (currentStr) {
+      rows.push(currentStr);
+    }
+    const lastRow = rows.slice(-1)[0];
+    const newLastRow = lastRow + ' '.repeat(40 - lastRow.length - this.formatPrice(price).length) + this.formatPrice(price);
+    rows[rows.length - 1] = newLastRow;
+    console.log(newLastRow);
+    return rows.join('\n');
+  },
 };
