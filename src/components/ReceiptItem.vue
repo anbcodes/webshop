d7b41f244c75568806c24c450b364d18fe0d2525
anbcodes <andrewbaumes@gmail.com>
2019-11-09 18:29:13 -0500

    Refractored scanner and fixed #10

---
+++ src/components/ReceiptItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-list-item style="font-family: monospace;">
+    <v-list-item-action>
+      <v-btn icon @click="$emit('remove', item)">
+        <v-icon>
+          mdi-minus
+        </v-icon>
+      </v-btn>
+    </v-list-item-action>
+    <v-list-item-content>
+      {{item.name}} ({{strPrice}}) x{{item.number}} - {{strTotal}}
+    </v-list-item-content>
+  </v-list-item>
+</template>
+<script>
+import formatter from '../util/Formatter';
+
+export default {
+  props: {
+    item: Object,
+  },
+
+  data: () => ({
+    formatter,
+  }),
+
+  computed: {
+    strPrice() {
+      return this.formatter.formatPrice(this.item.price);
+    },
+    strTotal() {
+      return this.formatter.formatPrice(+this.item.price * this.item.number);
+    },
+  },
+};
+</script>
