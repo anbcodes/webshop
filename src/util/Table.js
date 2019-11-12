1861973c736ac810028cad58314414a2ad5bb66d
anbcodes <anbcodes@protonmail.com>
2019-11-11 20:47:54 -0500

    added logging to table.js working on #18

--- src/util/Table.js
+++ src/util/Table.js
@@ -6,9 +6,16 @@ export default {
   sortBy: null,
   sortUp: null,
   init(onUpdate) {
-    Log('Initing table', { onUpdate, __filename });
     this.onUpdate = onUpdate;
     this.db = new Database('WebShopTable', onUpdate);
+    Log('Inited table', { db: this.db, onUpdate, __filename });
+  },
+
+  async erase() {
+    Log('Erasing items', {
+      __filename,
+    });
+    await this.db.erase();
   },
 
   get items() {
@@ -20,54 +27,9 @@ export default {
     return this.sortItems(this.db.items);
   },
 
-  async erase() {
-    Log('Erasing items', {
-      __filename,
-    });
-    await this.db.erase();
-  },
-
-  async addItem(item) {
-    console.log((await this.db.items), this);
-    const items = [...(await this.db.items)];
-    const itemCopy = { ...item };
-    const nextBarcodeId = Math.max(...items.map(v => v.barcodeId));
-    itemCopy.barcodeId = nextBarcodeId === -Infinity ? 0 : nextBarcodeId;
-    console.log('adding Items', items, itemCopy);
-
-    items.push(itemCopy);
-    await (this.db.items = items);
-  },
-
-  async removeItem(item) {
-    console.log('removing item', item);
-    let items = [...(await this.db.items)];
-    delete items[items
-      .map(v => v.barcodeId)
-      .indexOf(item.barcodeId)
-    ];
-    items = items.filter(v => v);
-    await (this.db.items = items);
-  },
-
-  async editItem(item) {
-    const items = [...(await this.db.items)];
-    console.log('ITEMS', items);
-    const index = items
-      .map(v => v.barcodeId)
-      .indexOf(item.barcodeId);
-    if (index === -1) {
-      console.log('Item Not Found', index, items, item);
-      return 'Item Not Found';
-    }
-    items[index] = item;
-    items.filter(v => v);
-    await (this.db.items = items);
-    return item;
-  },
-
   async sortItems(itemsPromise) {
     const items = await itemsPromise;
+    const originalItems = [...items];
     items.sort((a, b) => {
       if (a[this.sortBy] > b[this.sortBy]) {
         return this.sortUp ? 1 : -1;
@@ -79,21 +41,72 @@ export default {
 
       return 0;
     });
+    Log('Sorted Items', {
+      items,
+      originalItems,
+      sortUp: this.sortUp,
+      sortBy: this.sortBy,
+      __filename,
+    });
     return items;
   },
 
-  async updateSort(name) {
-    Log('updating sort', {
-      sortBy: this.sortBy,
-      sortUp: this.sortUp,
-      name,
+  async addItem(item) {
+    const items = [...(await this.db.items)];
+    const itemCopy = { ...item };
+    const nextBarcodeId = Math.max(...items.map(v => v.barcodeId));
+    itemCopy.barcodeId = nextBarcodeId === -Infinity ? 0 : nextBarcodeId;
+    items.push(itemCopy);
+    await (this.db.items = items);
+    Log('Added Item', { items, itemCopy });
+  },
+
+  async removeItem(item) {
+    let items = [...(await this.db.items)];
+    delete items[items
+      .map(v => v.barcodeId)
+      .indexOf(item.barcodeId)
+    ];
+    items = items.filter(v => v);
+    await (this.db.items = items);
+    Log('Removed item', { item, items });
+  },
+
+  async editItem(item) {
+    const items = [...(await this.db.items)];
+    const index = items
+      .map(v => v.barcodeId)
+      .indexOf(item.barcodeId);
+    if (index === -1) {
+      Log('Item Not Found', {
+        index, items, item, __filename,
+      });
+      return 'Item Not Found';
+    }
+    items[index] = item;
+    items.filter(v => v);
+    await (this.db.items = items);
+    Log('Edited Item', {
+      item,
+      index,
+      items,
       __filename,
     });
+    return item;
+  },
+
+  async updateSort(name) {
     if (this.sortBy === name) {
       this.sortUp = !this.sortUp;
     } else {
       this.sortBy = name;
     }
     this.onUpdate(await this.items);
+    Log('updated sort', {
+      sortBy: this.sortBy,
+      sortUp: this.sortUp,
+      name,
+      __filename,
+    });
   },
 };
