aecf373c5d298049fa837055ad36b2a4d461ecbc
anbcodes <andrewbaumes@gmail.com>
2019-11-11 21:17:28 -0500

    added logging to Scanner.js working on #18

--- src/util/Scanner.js
+++ src/util/Scanner.js
@@ -1,5 +1,5 @@
 import Quagga from 'quagga';
-
+import Log from './Log';
 
 export default {
   currentCallBack: null,
@@ -18,27 +18,38 @@ export default {
       },
     }, (err) => {
       if (err) {
-        console.log(err);
+        Log(__filename, 'ERROR: Starting scanning faild', {
+          err,
+          Quagga,
+          cbFrequency,
+          element,
+        });
         return;
       }
       Quagga.start();
+      Log(__filename, 'Starting scanning', { Quagga, cbFrequency, element });
     });
   },
 
   addCallback() {
     if (!this.currentCallback) {
       this.currentCallback = Quagga.onDetected(data => this.detection(data));
+      Log(__filename, 'Added callback', { callback: this.currentCallBack });
     }
+    Log(__filename, 'Error adding callback. Callback alread exists', { callback: this.currentCallBack });
   },
 
   onCode(cb) {
     this.cb = cb;
+    Log(__filename, 'Setting onCode callback', { cb });
   },
 
   detection(data) {
+    Log(__filename, 'Detection', { data }, true);
     if (this.withinFrequency) {
       return;
     }
+    Log(__filename, 'Detection after fecquency', { data });
     console.log(data);
     this.cb(data.codeResult.code);
     this.withinFrequency = true;
@@ -49,5 +60,6 @@ export default {
     Quagga.offDetected(this.currentCallback);
     Quagga.stop();
     this.currentCallback = null;
+    Log(__filename, 'Stoped scanning', { Quagga, cb: this.currentCallBack });
   },
 };
